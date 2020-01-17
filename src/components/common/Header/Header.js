1fc4dc36dabc09437ae9fd6654305bdfed3b44d9
LeeJaeBae <nea4182@gmail.com>
2020-01-18 03:52:11 +0900

    CSS 노가다

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,7 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import styled from "styled-components";
+import { colors, mixins, breakpoints } from "../../../styles/lib";
+
+const Head = styled.header`
+  background-color: ${colors.blue[6]};
+`;
+const Content = styled(Head)`
+  height: 5rem;
+  width: 1400px;
+  padding-left: 3rem;
+  padding-right: 3rem;
+  display: flex;
+  margin: 0 auto;
+  align-content: center;
+  @media (min-width: ${breakpoints.wide}) {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+  @media (min-width: ${breakpoints.medium}) {
+    width: 100%;
+  }
+`;
+const Brand = styled.div`
+  position: relative;
+  top: calc(50% - 0.75rem);
+  color: white;
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
+const Right = styled.div`
+  position: relative;
+  top: calc(50% - 0.5rem);
+  right: 0;
+  margin-left: auto;
+`;
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Head>
+        <Content as="div">
+          <Brand>
+            <Link to="/">BAELOG</Link>
+          </Brand>
+          <Right>RIGHT</Right>
+        </Content>
+      </Head>
+    </>
+  );
 };
 
 export default Header;
